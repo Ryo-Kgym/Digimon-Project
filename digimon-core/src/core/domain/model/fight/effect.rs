1cf3348edcbd27d10a044589b9b1a2c49527562e
Ryo-Kgym <mraytoh@icloud.com>
2022-10-02 01:08:14 +0900

    - Added the append method in Effects.

--- digimon-core/src/core/domain/model/fight/effect.rs
+++ digimon-core/src/core/domain/model/fight/effect.rs
@@ -16,11 +16,55 @@ pub struct Effects {
 }
 
 impl Effects {
-    pub fn build() -> Effects {
+    pub fn build() -> Self {
         let effects = Vec::new();
 
         Effects {
             effects
         }
+    }
+
+    pub fn append(self, mut added: Effects) -> Self {
+        let mut effects = self.effects;
+        effects.append(&mut added.effects);
+
+        Effects {
+            effects
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::core::domain::model::fight::effect::{Effect, Effects};
+    use crate::core::domain::model::fight::effect::EffectType::AttackMultiply;
+    use crate::core::domain::model::status::attribute::Attribute::{DATA, VACCINE, VIRUS};
+
+    #[test]
+    fn test_build() {
+        let actual = Effects::build();
+        let expected = Effects {
+            effects: Vec::new(),
+        };
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn test_append() {
+        let actual = Effects::of(VACCINE, VIRUS)
+            .append(Effects::of(DATA, VIRUS));
+        let expected = Effects {
+            effects: vec![
+                Effect {
+                    effect_type: AttackMultiply(2),
+                },
+                Effect {
+                    effect_type: AttackMultiply(1 / 2),
+                },
+            ],
+        };
+
+        assert_eq!(actual, expected)
     }
 }
