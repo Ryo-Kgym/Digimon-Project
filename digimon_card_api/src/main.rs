1d9c8b549ae5fd3d2e30ba6cfe3a76d84a68225e
Ryo-Kgym <mraytoh@icloud.com>
2022-10-02 23:31:02 +0900

    - Renamed the method name.

--- digimon_card_api/src/main.rs
+++ digimon_card_api/src/main.rs
@@ -3,20 +3,23 @@ use actix_web::web::Data;
 use async_graphql::{EmptySubscription, Schema};
 use async_graphql::http::{GraphQLPlaygroundConfig, playground_source};
 use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
-use query::MyQuery;
+
 use mutation::MyMutation;
+use query::MyQuery;
 
 mod query;
 mod mutation;
 mod photo;
 
 
-async fn index(schema: Data<Schema<MyQuery, MyMutation, EmptySubscription>>, req: GraphQLRequest) -> GraphQLResponse {
+async fn post_index(schema: Data<Schema<MyQuery, MyMutation, EmptySubscription>>,
+                    req: GraphQLRequest) -> GraphQLResponse {
     schema.execute(req.into_inner()).await.into()
 }
 
-async fn index_playground() -> Result<HttpResponse> {
-    let source = playground_source(GraphQLPlaygroundConfig::new("/").subscription_endpoint("/"));
+async fn get_index() -> Result<HttpResponse> {
+    let source = playground_source(GraphQLPlaygroundConfig::new("/")
+        .subscription_endpoint("/"));
     Ok(HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(source))
@@ -24,15 +27,16 @@ async fn index_playground() -> Result<HttpResponse> {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let schema = Schema::build(MyQuery, MyMutation, EmptySubscription).finish();
+    let schema = Schema::build(MyQuery, MyMutation, EmptySubscription)
+        .finish();
 
     println!("Playground: http://localhost:8000");
 
     HttpServer::new(move || {
         App::new()
             .app_data(Data::new(schema.clone()))
-            .service(web::resource("/").guard(guard::Post()).to(index))
-            .service(web::resource("/").guard(guard::Get()).to(index_playground))
+            .service(web::resource("/").guard(guard::Post()).to(post_index))
+            .service(web::resource("/").guard(guard::Get()).to(get_index))
     })
         .bind("127.0.0.1:8000")?
         .run()
