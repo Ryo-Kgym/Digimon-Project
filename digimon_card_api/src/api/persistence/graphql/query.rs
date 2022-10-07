b29117a9ae044c5118ff0d385001d8f3cf12af53
Ryo-Kgym <mraytoh@icloud.com>
2022-10-07 22:01:18 +0900

    - Pick Up the examples https://github.com/actix/examples/blob/99d0afde28d14a0b641ac52de821d79fa244d50a/graphql/juniper/src/main.rs

--- digimon_card_api/src/api/persistence/graphql/query.rs
+++ digimon_card_api/src/api/persistence/graphql/query.rs
@@ -1,17 +1,17 @@
-use async_graphql::Object;
-
-use crate::api::sample::photo::PHOTOS;
-
-pub struct DigimonQuery;
-
-
-#[Object]
-impl DigimonQuery {
-    async fn total_photos(&self) -> usize {
-        PHOTOS.lock().unwrap().len()
-    }
-
-    async fn test_query(&self) -> String {
-        String::from("test_query")
-    }
-}
+// use async_graphql::Object;
+//
+// use crate::api::sample::photo::PHOTOS;
+//
+// pub struct DigimonQuery;
+//
+//
+// #[Object]
+// impl DigimonQuery {
+//     async fn total_photos(&self) -> usize {
+//         PHOTOS.lock().unwrap().len()
+//     }
+//
+//     async fn test_query(&self) -> String {
+//         String::from("test_query")
+//     }
+// }
--- digimon_card_api/src/main.rs
+++ digimon_card_api/src/main.rs
@@ -1,23 +1,57 @@
-use actix_web::{App, guard, HttpServer, web};
-use actix_web::web::Data;
+//! Actix Web juniper example
+//!
+//! A simple example integrating juniper in Actix Web
 
-use api::config::graphql::{get_index, post_index, schema};
+use std::{io, sync::Arc};
 
-mod api;
+use actix_cors::Cors;
+use actix_web::{
+    get, middleware, route,
+    web::{self, Data},
+    App, HttpResponse, HttpServer, Responder,
+};
+use actix_web_lab::respond::Html;
+use juniper::http::{graphiql::graphiql_source, GraphQLRequest};
 
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    println!("Graph iQL is here: http://localhost:8000");
+mod schema;
 
+use crate::schema::{create_schema, Schema};
+
+/// GraphiQL playground UI
+#[get("/graphiql")]
+async fn graphql_playground() -> impl Responder {
+    Html(graphiql_source("/graphql", None))
+}
+
+/// GraphQL endpoint
+#[route("/graphql", method = "GET", method = "POST")]
+async fn graphql(st: web::Data<Schema>, data: web::Json<GraphQLRequest>) -> impl Responder {
+    let user = data.execute(&st, &()).await;
+    HttpResponse::Ok().json(user)
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    // Create Juniper schema
+    let schema = Arc::new(create_schema());
+
+    log::info!("starting HTTP server on port 8080");
+    log::info!("GraphiQL playground: http://localhost:8080/graphiql");
+
+    // Start HTTP server
     HttpServer::new(move || {
         App::new()
-            .app_data(Data::new(schema().clone()))
-            .service(web::resource("/")
-                .guard(guard::Post()).to(post_index))
-            .service(web::resource("/")
-                .guard(guard::Get()).to(get_index))
+            .app_data(Data::from(schema.clone()))
+            .service(graphql)
+            .service(graphql_playground)
+            // the graphiql UI requires CORS to be enabled
+            .wrap(Cors::permissive())
+            .wrap(middleware::Logger::default())
     })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    .workers(2)
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
