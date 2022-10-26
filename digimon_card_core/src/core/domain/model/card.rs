c12440be53eab80046b068e4bb803e107949ac4f
Ryo-Kgym <mraytoh@icloud.com>
2022-10-26 23:03:46 +0900

    - Removed the package.

--- digimon_card_core/src/core/domain/model/card.rs
+++ digimon_card_core/src/core/domain/model/card.rs
@@ -1,1 +1,41 @@
-pub mod card;
+use crate::core::domain::model::fight::effect::Effects;
+
+#[derive(PartialEq)]
+pub enum Card {
+	// アイテム
+	RecoveryFloppy,                   // 回復フロッピー
+	AttackPlugin,                     // 攻撃プラグイン
+}
+
+impl Card {
+	pub fn get_effects(&self) -> Effects {
+		match self {
+			Card::RecoveryFloppy => { Effects::of_recovery_effects(300) }
+			// TODO
+			Card::AttackPlugin => { Effects::empty() }
+		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use crate::core::domain::model::card::Card;
+	use crate::core::domain::model::fight::effect::Effects;
+
+	#[test]
+	fn test_get_effects() {
+		let actual_list = vec![
+			Card::RecoveryFloppy,
+			Card::AttackPlugin,
+		];
+
+		let expected_list = vec![
+			Effects::of_recovery_effects(300),
+			Effects::empty(),
+		];
+
+		for i in 0..actual_list.len() {
+			assert_eq!(actual_list[i].get_effects(), expected_list[i])
+		}
+	}
+}
--- digimon_card_core/src/core/domain/use_case.rs
+++ digimon_card_core/src/core/domain/use_case.rs
@@ -1,2 +1,3 @@
 pub mod attack;
 pub mod recovery;
+pub mod card;
