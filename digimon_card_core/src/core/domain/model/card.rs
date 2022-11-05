508b9f7ef8c1e41d8beb8a53038b9dcf55c793ea
Ryo-Kgym <mraytoh@icloud.com>
2022-11-06 00:51:00 +0900

    - Changed the structure.

--- digimon_card_core/src/core/domain/model/card.rs
+++ digimon_card_core/src/core/domain/model/card.rs
@@ -18,9 +18,10 @@ pub enum Card {
 impl Card {
     pub fn get_effects(&self) -> Effects {
         match self {
+            Card::AguMon => { Effects::empty() }
+            Card::PicoDevilMon => { Effects::empty() }
             Card::RecoveryFloppy => { Effects::of_recovery_effects(300) }
             Card::AttackPlugin => { Effects::of_attack_plus(100) }
-            _ => { Effects::empty() }
         }
     }
 
@@ -28,7 +29,8 @@ impl Card {
         match self {
             Card::AguMon => { Some(fetch_agu_mon_parameter()) }
             Card::PicoDevilMon => { Some(fetch_pico_devil_mon_parameter()) }
-            _ => { None }
+            Card::RecoveryFloppy => { None }
+            Card::AttackPlugin => { None }
         }
     }
 }
@@ -41,7 +43,7 @@ mod tests {
     use crate::core::domain::model::fight::effect::Effects;
 
     #[test]
-    fn test_get_effects() {
+    fn test_get() {
         let actual_list = vec![
             Card::AguMon,
             Card::PicoDevilMon,
@@ -49,28 +51,14 @@ mod tests {
             Card::AttackPlugin,
         ];
 
-        let expected_list = vec![
+        let effects_expected_list = vec![
             Effects::empty(),
             Effects::empty(),
             Effects::of_recovery_effects(300),
             Effects::of_attack_plus(100),
         ];
 
-        for i in 0..actual_list.len() {
-            assert_eq!(actual_list[i].get_effects(), expected_list[i])
-        }
-    }
-
-    #[test]
-    fn test_get_digimon() {
-        let actual_list = vec![
-            Card::AguMon,
-            Card::PicoDevilMon,
-            Card::RecoveryFloppy,
-            Card::AttackPlugin,
-        ];
-
-        let expected_list = vec![
+        let digimon_expected_list = vec![
             Some(fetch_agu_mon_parameter()),
             Some(fetch_pico_devil_mon_parameter()),
             None,
@@ -78,7 +66,8 @@ mod tests {
         ];
 
         for i in 0..actual_list.len() {
-            assert_eq!(actual_list[i].get_digimon(), expected_list[i])
+            assert_eq!(actual_list[i].get_effects(), effects_expected_list[i]);
+            assert_eq!(actual_list[i].get_digimon(), digimon_expected_list[i]);
         }
     }
 }
